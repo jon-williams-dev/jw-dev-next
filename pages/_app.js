f2591ef7970a2e03676466bf98adfe4f47e05194
Jon Williams <jon.williams@hey.com>
2021-08-29 23:12:06 +0200

    fixing vercel fa issue

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@
 // import here for global compponent
 import '../styles/global.scss'
 
-import { config } from '@fortawesome/fontawesome-svg-core'
+// import { config } from '@fortawesome/fontawesome-svg-core'
 // import '@fortawesome/fontawesome-svg-core/styles.css'
 
 // gor stlyes per compoennt - create css modules 

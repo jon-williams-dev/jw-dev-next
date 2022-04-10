3f4b8ddca9ec1a73550119b9182b1a9106b15f99
Jon Williams <jon.williams@hey.com>
2022-04-10 20:36:05 +0200

    garage plans

--- pages/bikes.js
+++ pages/bikes.js
@@ -1,59 +1,59 @@
-import {gql} from '@apollo/client'
-import Head from 'next/head' 
-import Link from 'next/link'
-import client from '../apolloClient'
+// import {gql} from '@apollo/client'
+// import Head from 'next/head' 
+// import Link from 'next/link'
+// import client from '../apolloClient'
 
-export default function Bikes({bikes}) {
+// export default function Bikes({bikes}) {
 
-  console.log(bikes);
+//   console.log(bikes);
 
-  return (
-    <div>
-      <Head>
-        <title>Jon Williams | Bikes</title>
-      </Head>
-      <h1>Bikes</h1>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <Head>
+//         <title>Jon Williams | Bikes</title>
+//       </Head>
+//       <h1>Bikes</h1>
+//     </div>
+//   )
+// }
 
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
 
-  const {data: bikes} = await client.query({
-    query: gql`
-      query {
-        bikes {
-          title
-          slug
-          make
-          model
-          year
-          mileage
-          price
-          color
-          header
-          quote
-          description {
-            markdown
-          }
-          image {
-            url
-          }
-        }
-      }
+//   const {data: bikes} = await client.query({
+//     query: gql`
+//       query {
+//         bikes {
+//           title
+//           slug
+//           make
+//           model
+//           year
+//           mileage
+//           price
+//           color
+//           header
+//           quote
+//           description {
+//             markdown
+//           }
+//           image {
+//             url
+//           }
+//         }
+//       }
         
-    `
-  })
+//     `
+//   })
 
-  console.log(bikes);
+//   console.log(bikes);
 
-  return {
-    props: {
-      bikes
-    }
-  }
-}
+//   return {
+//     props: {
+//       bikes
+//     }
+//   }
+// }
 
 
 
